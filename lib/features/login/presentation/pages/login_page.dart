4d7f9896382d5ddc712639086d1c4f97f4357249
Geraldine <aJ1094816@admin.hubtotal.net>
2023-12-05 23:32:24 +0100

    Login Feature

---
+++ lib/features/login/presentation/pages/login_page.dart
@@ -0,0 +1,6 @@
+//TODO the goal is to organise the splash screen and the login
+
+//TODO see AppBloc of manager ->main bloc
+//TODO see ManagerApp -> is where Appbloc is implemented
+
+//TODO https://www.youtube.com/watch?v=uOTaHoB-YmA&t=1s -> firebase Login.
